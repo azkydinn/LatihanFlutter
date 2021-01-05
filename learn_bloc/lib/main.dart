11859535421a5ca7df136f694686d467483de534
azkydinn <azky.dinn@gmail.com>
2021-01-05 21:43:45 +0700

    Belajar BLoC tanpa Library

---
+++ learn_bloc/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:learn_bloc/color_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ColorBloc bloc = ColorBloc();
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          floatingActionButton: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FloatingActionButton(
+                  onPressed: () {
+                    bloc.eventSink.add(ColorEvent.to_amber);
+                  },
+                  backgroundColor: Colors.amber),
+              SizedBox(width: 10),
+              FloatingActionButton(
+                  onPressed: () {
+                    bloc.eventSink.add(ColorEvent.to_light_blue);
+                  },
+                  backgroundColor: Colors.lightBlue)
+            ],
+          ),
+          appBar: AppBar(
+            title: Text("BLoC tanpa Library"),
+          ),
+          body: Center(
+            child: StreamBuilder(
+                stream: bloc.stateStream,
+                initialData: Colors.amber,
+                builder: (context, snapshot) {
+                  return AnimatedContainer(
+                      duration: Duration(milliseconds: 1500),
+                      width: 100,
+                      height: 100,
+                      color: snapshot.data);
+                }),
+          )),
+    );
+  }
+}
